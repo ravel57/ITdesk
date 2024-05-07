b9584f538292fc646647dc27736388605f5857a2
petr <petyalom@yandex.ru>
2024-05-07 07:54:57 +0300

    script fix

--- build_front.sh
+++ build_front.sh
@@ -6,16 +6,16 @@ cd ../../WebstormProjects/itdesk-front || exit
 yarn install
 yarn build
 
-if [ -d "./dist/" ]; then
+if [ -d "./dist/spa/" ]; then
     rm -rf "$path/src/main/webapp/js"
     rm -rf "$path/src/main/webapp/css"
     rm -rf "$path/src/main/webapp/icons"
     rm -rf "$path/src/main/webapp/fonts"
-    cp -r "./dist/"* "$path/src/main/webapp/"
+    cp -r "./dist/spa/"* "$path/src/main/webapp/"
 
     cd "$path/src/main/webapp/js" || exit
     mv app.*.js app.js
-    mv vendors.*.js vendors.js
+    mv vendor.*.js vendor.js
 
     cd "$path/src/main/webapp/css" || exit
     mv app.*.css app.css

1cd4023356cdc67828a43c01b5ef8fd6747b75e2
petr <petyalom@yandex.ru>
2024-05-12 05:31:17 +0300

    fixed driver for flyway

--- src/main/java/ru/ravel/ItDesk/config/FlywayConfig.java
+++ src/main/java/ru/ravel/ItDesk/config/FlywayConfig.java
@@ -34,6 +34,7 @@ public class FlywayConfig {
 	@Bean
 	public DataSource dataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.postgresql.Driver");
 		dataSource.setUrl(url);
 		dataSource.setUsername(username);
 		dataSource.setPassword(password);

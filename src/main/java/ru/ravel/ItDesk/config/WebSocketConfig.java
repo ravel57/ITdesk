f766131a9d39c68b23adb0c22e95fa83bdf6485c
petr <petyalom@yandex.ru>
2024-06-04 23:47:31 +0300

    test nginx 2; added environments

--- src/main/java/ru/ravel/ItDesk/config/WebSocketConfig.java
+++ src/main/java/ru/ravel/ItDesk/config/WebSocketConfig.java
@@ -1,8 +1,8 @@
 package ru.ravel.ItDesk.config;
 
 import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -14,6 +14,10 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @CrossOrigin
 class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+	@Value("${allowed-origin}")
+	private String allowedOriginPatterns;
+
+
 	@Override
 	public void configureMessageBroker(@NotNull MessageBrokerRegistry config) {
 		config.enableSimpleBroker("/topic");
@@ -22,7 +26,7 @@ class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void registerStompEndpoints(@NotNull StompEndpointRegistry registry) {
-		registry.addEndpoint("/ws").setAllowedOriginPatterns("http://172.22.0.20:56481").withSockJS();
+		registry.addEndpoint("/ws").setAllowedOriginPatterns(allowedOriginPatterns).withSockJS();
 	}
 
 }

654d2ddd0789e5fa7be1a7382c224a1609f8c34e
petr <petyalom@yandex.ru>
2024-07-20 17:47:21 +0300

    fix WebSocketConfig

--- src/main/java/ru/ravel/ItDesk/config/WebSocketConfig.java
+++ src/main/java/ru/ravel/ItDesk/config/WebSocketConfig.java
@@ -2,12 +2,14 @@ package ru.ravel.ItDesk.config;
 
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -24,9 +26,19 @@ class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 		config.setApplicationDestinationPrefixes("/app");
 	}
 
+
 	@Override
 	public void registerStompEndpoints(@NotNull StompEndpointRegistry registry) {
 		registry.addEndpoint("/ws").setAllowedOriginPatterns(allowedOriginPatterns).withSockJS();
+	}
+
+
+	@Bean
+	public ServletServerContainerFactoryBean createServletServerContainerFactoryBean() {
+		ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+		container.setMaxTextMessageBufferSize(32768);
+		container.setMaxBinaryMessageBufferSize(32768);
+		return container;
 	}
 
 }

2a7f1eae50d4739df070efb9fcc55037c15531c7
petr <petyalom@yandex.ru>
2024-07-25 23:54:59 +0300

    added unfroze task on time on

--- src/main/java/ru/ravel/ItDesk/service/SchedulerService.java
+++ src/main/java/ru/ravel/ItDesk/service/SchedulerService.java
@@ -6,7 +6,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import ru.ravel.ItDesk.component.LicenseStarter;
+import ru.ravel.ItDesk.repository.TaskRepository;
 
+import java.time.ZonedDateTime;
 import java.util.concurrent.TimeUnit;
 
 
@@ -18,6 +20,7 @@ public class SchedulerService {
 	private final WebSocketService webSocketService;
 	private final UserService userService;
 	private final LicenseStarter licenseStarter;
+	private final TaskRepository taskRepository;
 
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
@@ -32,6 +35,17 @@ public class SchedulerService {
 	@Scheduled(fixedRate = 1, timeUnit = TimeUnit.HOURS)
 	private void checkLicense() {
 		licenseStarter.run();
+	}
+
+
+	@Scheduled(fixedRate = 1, timeUnit = TimeUnit.MINUTES)
+	private void checkFrozenTasks(){
+		taskRepository.findAll().stream()
+				.filter(task -> task.getFrozen() != null && task.getFrozen())
+				.filter(task -> ZonedDateTime.now().isAfter(task.getFrozenUntil()))
+				.peek(task -> task.setFrozen(false))
+				.peek(task -> task.setFrozenUntil(null))
+				.forEach(taskRepository::save);
 	}
 
 }

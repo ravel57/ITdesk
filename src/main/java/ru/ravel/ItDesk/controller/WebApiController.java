d57429e2d119e03a33ed60a8ef65d9dbb21b059d
petr <petyalom@yandex.ru>
2024-08-07 07:12:10 +0300

    denied update and delete hardcoded statuses

--- src/main/java/ru/ravel/ItDesk/controller/WebApiController.java
+++ src/main/java/ru/ravel/ItDesk/controller/WebApiController.java
@@ -447,7 +447,11 @@ public class WebApiController {
 	@PreAuthorize("hasAnyRole('ADMIN')")
 	public ResponseEntity<Object> updateStatus(@RequestBody Status status) {
 		if (LicenseStarter.isLicenseActive) {
-			return ResponseEntity.ok().body(statusService.updateStatus(status));
+			try {
+				return ResponseEntity.ok().body(statusService.updateStatus(status));
+			} catch (Exception e) {
+				return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+			}
 		} else {
 			return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
 		}
@@ -461,7 +465,7 @@ public class WebApiController {
 			try {
 				statusService.deleteStatus(statusId);
 			} catch (Exception e) {
-				ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+				return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
 			}
 			return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 		} else {
